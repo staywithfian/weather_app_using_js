dc26e8e7b9f650a4e8a28c07c99b17e3ef71a2b4
staywithfian <22103154@iubat.edu>
2026-07-01 21:50:34 +0600

    Add files via upload

---
+++ weather_app/base.js
@@ -0,0 +1,31 @@
+document.getElementById('btn').addEventListener('click',() =>{
+const apikey = "f68d02a8db98277633a258a716430431";
+const inpvalue = document.getElementById('input').value;
+const api = `https://api.openweathermap.org/data/2.5/weather?q=${inpvalue}&appid=${apikey}&units=metric`;
+
+fetch(api)
+.then(Response => Response.json()) 
+.then(data =>{
+
+const des = data.weather[0].description;
+const tmp = data.main.temp;
+const humd = data.main.humidity;
+const ws = data.wind.speed;
+
+const div = document.getElementById('div');
+
+div.innerHTML = `
+<h2>📍 ${inpvalue}</h2>
+
+<p>Descriptions: ${des}</p>
+<p>Temperature: ${tmp} &#8451</p>
+<p>Humidity: ${humd} %</p>
+<p>Wind Speed: ${ws} m/s</p>
+`;
+
+}).catch(error =>{
+ console.error('An Error Occured',error);
+  document.getElementById('div').innerText="City Not Found";
+})
+
+})
